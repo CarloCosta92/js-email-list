0fa22ecded2df15f8c9dd46427aa544c1c74dc2a
Carlo Costa <carlo.costa92@gmail.com>
2025-03-13 14:32:44 +0100

    feat: function to generate ten calls

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // console.log("carlo")
+
+// Descrizione
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+// funzione per generare 10 chiamate all api
+
+function generaEmail (){
+
+    for (let i = 0; i < 10; i++) {
+        axios.get(endpoint)
+        .then(response => { 
+        console.log(response.data.response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+};
+
+
+
